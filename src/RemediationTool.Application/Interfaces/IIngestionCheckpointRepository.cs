8ba5df475d188c67f595e85f7a2df4c2bf4a6c35
srinu <srinubsv13@gmail.com>
2026-07-17 17:43:20 +0530

    Remove resume checkpoint reads

--- src/RemediationTool.Application/Interfaces/IIngestionCheckpointRepository.cs
+++ src/RemediationTool.Application/Interfaces/IIngestionCheckpointRepository.cs
@@ -1,10 +1,8 @@
-﻿using RemediationTool.Domain.Entities;
+using RemediationTool.Domain.Entities;
 
 namespace RemediationTool.Application.Interfaces;
 
 public interface IIngestionCheckpointRepository
 {
-    IngestionCheckpoint? GetByJobId(string jobId);
-
     void Upsert(IngestionCheckpoint checkpoint);
 }
