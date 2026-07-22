5e0e05d7e5a948fd1010d83f66c56d7efdd96581
srinu <srinubsv13@gmail.com>
2026-07-22 14:45:51 +0530

    Correct ingestion service contract for resume flow

--- src/RemediationTool.Application/Interfaces/IIngestionService.cs
+++ src/RemediationTool.Application/Interfaces/IIngestionService.cs
@@ -1,15 +1,14 @@
-using Microsoft.AspNetCore.Http;
 using RemediationTool.Application.Models;
 
 namespace RemediationTool.Application.Interfaces;
 
 public interface IIngestionService
 {
-    Task<IngestionUploadResponse> ProcessAsync(
-        IFormFile file,
+    Task<IngestionUploadResponse> IngestAsync(
+        string reportUid,
         CancellationToken cancellationToken = default);
 
     Task<IngestionUploadResponse> ResumeAsync(
-        string jobId,
+        string reportUid,
         CancellationToken cancellationToken = default);
 }
